c52e41615ba3119903595545030a116e27305e04
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2021-12-27 17:26:28 +0530

    sprite animation

--- Flappy Bird/Assets/Scripts/Player.cs
+++ Flappy Bird/Assets/Scripts/Player.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
     private Vector3 direction;
 
     public float gravity = -9.8f;
     public float strength = 5f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
 
     private void Update()
     {
@@ -18,5 +32,17 @@ public class Player : MonoBehaviour
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if(spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
